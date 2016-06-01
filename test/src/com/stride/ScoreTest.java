380c6bca9b62885b114a66ba9fa7c4eeabbc6826
Conrad Benham <conrad@stride.nyc>
2016-06-01 16:57:07 -0400

    Removed duplication in test.

--- test/src/com/stride/ScoreTest.java
+++ test/src/com/stride/ScoreTest.java
@@ -1,27 +1,33 @@
 package com.stride;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ScoreTest {
 
-    @Test
-    public void shouldHaveAnInitialScore() {
-        assertEquals(501, new Score().score());
+    private Score score;
+
+    @Before
+    public void setUp() throws Exception {
+        score = new Score();
     }
 
     @Test
-    public void shouldReduceScoreForFirstSingleLaunch() {
-        Score score = new Score();
+    public void shouldHaveAnInitialScore() {
+        assertEquals(501, score.score());
+    }
+
+    @Test
+    public void shouldReduceScoreForFirstSingleScoringLaunch() {
         score.launch("S4");
         assertEquals(497, score.score());
     }
 
     @Test
     public void shouldNotReduceScoreWhenFirstLaunchIsAMiss() {
-        Score score = new Score();
         score.launch("MISS");
         assertEquals(501, score.score());
     }
