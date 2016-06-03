3ebb09c2ec22916837b6324fdc3111ddc29992ab
Conrad Benham <conrad@stride.nyc>
2016-06-03 13:28:03 -0400

    Optimised imports

--- test/src/com/stride/ScoreTest.java
+++ test/src/com/stride/ScoreTest.java
@@ -2,8 +2,6 @@ package com.stride;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import junitparams.converters.Param;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
