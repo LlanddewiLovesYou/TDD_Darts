e9071e5fc6de874abf694cf6d18907a897d0a7e2
Conrad Benham <conrad@stridenyc.com>
2016-07-07 12:20:35 -0400

    Added stub score class

---
+++ ruby/lib/game/score.rb
@@ -0,0 +1,8 @@
+require 'models/miss'
+require 'models/pass'
+require 'models/single_throw'
+require 'models/double_throw'
+require 'models/triple_throw'
+
+class Score
+end
---
+++ ruby/spec/game/score_spec.rb
@@ -0,0 +1,10 @@
+require 'game/score'
+require 'models/miss'
+require 'models/pass'
+require 'models/single_throw'
+require 'models/double_throw'
+require 'models/triple_throw'
+
+describe Score do
+  subject { Score.new }
+end
