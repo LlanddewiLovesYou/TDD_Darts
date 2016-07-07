d472c2f1cd5670bb98e1d284a715f35eda4674db
Conrad Benham <conrad@stridenyc.com>
2016-07-07 12:46:51 -0400

    Added double throw

---
+++ java/src/main/java/com/stride/models/DoubleThrow.java
@@ -0,0 +1,18 @@
+package com.stride.models;
+
+public class DoubleThrow {
+    public static final int MULTIPLIER = 2;
+
+    private int score;
+
+    public DoubleThrow(int score) {
+        if (score < 1 || score > 20) {
+            throw new IllegalArgumentException("Throw score must be in range 1..20");
+        }
+        this.score = score * MULTIPLIER;
+    }
+
+    public int score() {
+        return score;
+    }
+}
---
+++ java/src/test/java/com/stride/models/DoubleThrowTest.java
@@ -0,0 +1,32 @@
+package com.stride.models;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+
+public class DoubleThrowTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void shouldDoubleAValidScore() {
+        assertEquals(20, new DoubleThrow(10).score());
+    }
+
+    @Test
+    public void shouldRaiseExceptionForScoreJustBelowMinimumInRange() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Throw score must be in range 1..20");
+        new DoubleThrow(0);
+    }
+
+    @Test
+    public void shouldRaiseExceptionForScoreJustAboveMaximumInRange() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Throw score must be in range 1..20");
+        new DoubleThrow(21);
+    }
+}
