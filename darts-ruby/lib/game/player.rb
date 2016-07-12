e3c176caecd613fd1996579aeb82d41c09e2e60a
Conrad Benham <conrad@stridenyc.com>
2016-07-12 14:23:16 -0400

    Added skeleton methods

--- darts-ruby/lib/game/player.rb
+++ darts-ruby/lib/game/player.rb
@@ -8,4 +8,13 @@ require File.join('models', 'throw_inner_bullseye')
 require File.join('models', 'throw_outer_bullseye')
 
 class Player
+
+  def score
+    raise NotImplementedError.new 'Method not implemented'
+  end
+
+  def turn(firstThrow, secondThrow, thirdThrow)
+    raise NotImplementedError.new 'Method not implemented'
+  end
+
 end
