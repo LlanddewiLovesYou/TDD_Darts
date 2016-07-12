84be3cf7cca88a91b47112a13a2bb7c63fb1a77e
Conrad Benham <conrad@stridenyc.com>
2016-07-12 16:51:26 -0400

    Refactors subjects out

--- darts-ruby/spec/models/throw/miss_spec.rb
+++ darts-ruby/spec/models/throw/miss_spec.rb
@@ -1,10 +1,7 @@
 require File.join('models', 'throw', 'miss')
 
 describe Throw::Miss do
-
-  subject { Throw::Miss.new }
-
   it 'has a score of zero' do
-    expect(subject.bed_score).to eq(0)
+    expect(Throw::Miss.new.bed_score).to eq(0)
   end
 end
--- darts-ruby/spec/models/throw/pass_spec.rb
+++ darts-ruby/spec/models/throw/pass_spec.rb
@@ -1,9 +1,7 @@
 require File.join('models', 'throw', 'pass')
 
 describe Throw::Pass do
-  subject { Throw::Pass.new }
-
   it 'has a score of zero' do
-    expect(subject.bed_score).to eq(0)
+    expect(Throw::Pass.new.bed_score).to eq(0)
   end
 end
