a6bb8dd87f35fa7984c7584dc0adfb52f711b9ab
Chris Aporta and Conrad Benham <conrad@stridenyc.com>
2017-06-13 21:22:03 -0400

    Validate upper bound of allowable bed values in Single

--- javascript/app/darts/bed/single.js
+++ javascript/app/darts/bed/single.js
@@ -1,6 +1,6 @@
 class Single {
     constructor(bedValue) {
-        if (bedValue < 1) {
+        if (bedValue < 1 || bedValue > 20) {
             throw new RangeError('Bed score must be in range 1..20');
         }
         this.bedValue = bedValue;
--- javascript/spec/darts/bed/single_spec.js
+++ javascript/spec/darts/bed/single_spec.js
@@ -9,5 +9,9 @@ describe('Single', () => {
         it('it should raise exception when created with bed score just below minimum in range', () => {
             expect(() => { new Single(0) }).toThrowError(RangeError, 'Bed score must be in range 1..20');
         });
+
+        it('it should raise exception when created with bed score just above maximum in range', () => {
+            expect(() => { new Single(21) }).toThrowError(RangeError, 'Bed score must be in range 1..20');
+        });
     });
 });
